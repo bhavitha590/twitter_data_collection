eba1606cb0c456d152054e1f6d7b633e1ea1e752
bhavitha590 <dbhavitha@gmail.com>
2017-11-18 23:43:11 -0500

    webpage is working now

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
-from flask import Flask,render_template
 from pymongo import MongoClient
-import os
-import webbrowser
-import json
-from bson import json_util
+from bson.objectid import ObjectId
+from flask import Flask,render_template,jsonify,json,request
+from fabric.api import *
 
 application = Flask(__name__)
 
 client = MongoClient('localhost:27017')
+db = client.twitterdb_1
 
-db = client.twitter
-cursor = db.random.find({})
+@application.route("/gettwitterdata",methods = ['POST'])
+def gettwitterdata():
+	try:
+		collection = db.twitter_search.find()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-webbrowser.open('file://' + os.path.join(BASE_DIR, 'list.html'))
-#@application.route("/")
-#def getList():
-	#return render_template('test.html')
-@application.route('/twitters', methods = ['GET'])
-def getMachineList():
-	    return json.dumps(cursor, default = json_util.default)
-	    #return jsonify()
-	    #return render_template('list.html')
+		collection_list = []
+		for data in collection:
+			print(data)
+			collection_item = {
+				'text' : data['text'],
+				'image_url' : data['image_url'],
+				'location' : data['location'],
+				'created_at' : data['created_at']
+				#'twitter_url' : 'https://twitter.com/barakobama/status/'+ data['_id']
+			}
+			collection_list.append(collection_item)
+	except e:
+		return str(e)
+	return json.dumps(collection_list)
+
+@application.route('/')
+def showMachineList():
+    return render_template('sample.html')	
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')    
